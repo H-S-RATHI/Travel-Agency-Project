7d5816ad7004e78884dce0ccb05e81849584d3a9
H.S.RATHI <goluu.000111@gmail.com>
2024-04-29 08:35:08 +0530

    Home Page Complete

---
+++ frontend/src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import Slider from "react-slick";
+import ava01 from "../../assets/images/ava-1.jpg";
+import ava02 from "../../assets/images/ava-2.jpg";
+import ava03 from "../../assets/images/ava-3.jpg";
+
+const Testimonials = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <Slider {...settings}>
+      <div className="testimonials">
+        <h1>Testimonials</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id
+          veritatis doloremque dolorem, eos perspiciatis deserunt saepe
+          distinctio dicta non. Itaque, modi repellat unde quidem ratione sequi
+          perspiciatis eum provident.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava01} className="w-25 h-25 rounded" alt="" />
+          <div className="">
+            <h5 className="mb-0 mt-3">Jhon Doe</h5>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h1>Testimonials</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id
+          veritatis doloremque dolorem, eos perspiciatis deserunt saepe
+          distinctio dicta non. Itaque, modi repellat unde quidem ratione sequi
+          perspiciatis eum provident.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava02} className="w-25 h-25 rounded" alt="" />
+          <div className="">
+            <h5 className="mb-0 mt-3">Lia Franklin</h5>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h1>Testimonials</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id
+          veritatis doloremque dolorem, eos perspiciatis deserunt saepe
+          distinctio dicta non. Itaque, modi repellat unde quidem ratione sequi
+          perspiciatis eum provident.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava03} className="w-25 h-25 rounded" alt="" />
+          <div className="">
+            <h5 className="mb-0 mt-3">Jhon Doe</h5>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h1>Testimonials</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id
+          veritatis doloremque dolorem, eos perspiciatis deserunt saepe
+          distinctio dicta non. Itaque, modi repellat unde quidem ratione sequi
+          perspiciatis eum provident.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava01} className="w-25 h-25 rounded" alt="" />
+          <div className="">
+            <h5 className="mb-0 mt-3">Jhon Doe</h5>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h1>Testimonials</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id
+          veritatis doloremque dolorem, eos perspiciatis deserunt saepe
+          distinctio dicta non. Itaque, modi repellat unde quidem ratione sequi
+          perspiciatis eum provident.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava02} className="w-25 h-25 rounded" alt="" />
+          <div className="">
+            <h5 className="mb-0 mt-3">Lia Franklin</h5>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h1>Testimonials</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id
+          veritatis doloremque dolorem, eos perspiciatis deserunt saepe
+          distinctio dicta non. Itaque, modi repellat unde quidem ratione sequi
+          perspiciatis eum provident.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img src={ava03} className="w-25 h-25 rounded" alt="" />
+          <div className="">
+            <h5 className="mb-0 mt-3">Jhon Doe</h5>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default Testimonials;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,60 +1,160 @@
-import React from 'react'
-import '../styles/home.css'
-import {Container, Row,Col} from 'reactstrap'
-import heroImg from '../assets/images/hero-img01.jpg'
-import heroImg02 from '../assets/images/hero-img02.jpg'
-import heroVideo from '../assets/images/hero-video.mp4'
-import worldImg from '../assets/images/world.png'
-import Subtitle from '../shared/Subtitle'
-import SearchBar from '../shared/SearchBar'
+import React from "react";
+import "../styles/home.css";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import experienceImg from "../assets/images/experience.png";
+import Subtitle from "../shared/Subtitle";
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+import Testimonials from "../components/Testimonials/Testimonials";
+import NewsLetter from "../shared/NewsLetter";
 const Home = () => {
-  return (<>
-  {/*================ Hero Section Start================ */}
-  <section>
-    <Container>
-      <Row>
-        <Col lg='6'>
-          <div className="hero__content">
-              <div className="hero__subtitle d-flex align-items-center">
-                <Subtitle subtitle={'know Before you Go'}/>
-                <img src={worldImg} alt="" />
+  return (
+    <>
+      {/*================ Hero Section Start================ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"know Before you Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight">Memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Dolor in nisi mollitia qui vitae optio facere debitis
+                  accusantium omnis maiores quod non soluta similique
+                  voluptates, tempore praesentium saepe perferendis obcaecati!
+                </p>
               </div>
-              <h1>Traveling opens the door to creating <span className="highlight">Memories</span></h1>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor in nisi mollitia qui vitae optio facere debitis accusantium omnis maiores quod non soluta similique voluptates, tempore praesentium saepe perferendis obcaecati!</p>
-          </div>
-        </Col>
-        <Col lg='2'>
-          <div className="hero__img-box">
-            <img src={heroImg} alt=""/>
-            </div> 
-        </Col>
-        <Col lg='2'>
-          <div className="hero__img-box mt-4">
-            <video src={heroVideo} alt="" controls/>
-            </div> 
-        </Col>
-        <Col lg='2'>
-          <div className="hero__img-box mt-5">
-            <img src={heroImg02} alt=""/>
-            </div> 
-        </Col>
-        <SearchBar/>
-      </Row>
-    </Container>
-  </section>
-  <section>
-    <Container>
-      <Row>
-        <Col lg="3">
-          <h5 className="services__subtitle">What we Serve</h5>
-          <h2 className="services__title">we offer our best Services</h2>
-        </Col>
-      </Row>
-    </Container>
-  </section>
-  </>
-    
-  )
-}
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we Serve</h5>
+              <h2 className="services__title">we offer our best Services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
 
-export default Home
+      {/* ================= Features Tour Section start ================  */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our Featured Tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+      {/* ================= Features Tour Section end   ================  */}
+      {/*================== experience section start    =============== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="mb-4">
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"} />
+                <h2>
+                  With Our All Experience <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br /> Voluptates, maxime. vbsdvsd aavsdvs.
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successful trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/*================== experience section end  =============== */}
+
+      {/*================== Gallery section start  =============== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery__title">
+                Visit our customers tour gallery
+              </h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImagesGallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/*================== Gallery section end  =============== */}
+
+      {/*================== Testimonial section start  =============== */}
+      <section>
+        <Row>
+          <Col lg="12">
+            <Subtitle subtitle={"Fans Love"} />
+            <h2 className="testimonial__title">What our fans say about us</h2>
+          </Col>
+          <Testimonials />
+        </Row>
+      </section>
+      {/*================== Testimonial section end  =============== */}
+      <NewsLetter />
+    </>
+  );
+};
+
+export default Home;
